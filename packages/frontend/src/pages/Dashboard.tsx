4abfffdf3f7bf26d8ef4944e78e3e05b48c11304
Rajat Bansal <13465354+rajatb-git@users.noreply.github.com>
2026-04-25 12:46:23 -0500

    updates

--- packages/frontend/src/pages/Dashboard.tsx
+++ packages/frontend/src/pages/Dashboard.tsx
@@ -1,83 +1,86 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-import apis from '@/api';
-import { HoldingAggregate } from '@/api/dashboard';
-import DashboardTable from '@/components/DashboardTable/DashTable';
-import WatchlistSection from '@/components/WatchlistSection';
-import { IAccount } from '@/models/AccountsModel';
-import { Column } from '@/types';
+import apis from "@/api";
+import type { HoldingAggregate } from "@/api/dashboard";
+import DashboardTable from "@/components/DashboardTable/DashTable";
+import WatchlistSection from "@/components/WatchlistSection";
+import type { IAccount } from "@/models/AccountsModel";
+import type { Column } from "@/types";
 
 export default function Dashboard() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [accounts, setAccounts] = React.useState<Array<IAccount>>([]);
-  const [dashboardData, setDashboardData] = React.useState<Array<HoldingAggregate>>([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [accounts, setAccounts] = React.useState<Array<IAccount>>([]);
+	const [dashboardData, setDashboardData] = React.useState<
+		Array<HoldingAggregate>
+	>([]);
 
-  const columns: Array<Column> = [
-    {
-      id: 'symbol',
-      label: 'SYM',
-    },
-    {
-      id: 'percentChange',
-      label: 'Change',
-      align: 'right',
-    },
-    {
-      id: 'totalGLPercent',
-      label: 'Total G/L',
-      align: 'right',
-    },
-    {
-      id: 'currentPrice',
-      label: 'Current Price',
-      align: 'right',
-    },
-    { id: 'marketValue', label: 'Market Value', align: 'right' },
-    { id: 'accountPercent', label: '% of Account', align: 'right' },
-    { id: 'accountId', label: 'Owner' },
-    { id: '', label: 'Recommendation' },
-    { id: '', label: '' },
-  ];
+	const columns: Array<Column> = [
+		{
+			id: "symbol",
+			label: "SYM",
+		},
+		{
+			id: "currentPrice",
+			label: "Current Price",
+			align: "right",
+		},
+		{
+			id: "percentChange",
+			label: "Change",
+			align: "right",
+		},
+		{
+			id: "totalGLPercent",
+			label: "Total G/L",
+			align: "right",
+		},
 
-  const loadData = async () => {
-    setIsLoading(true);
+		{ id: "marketValue", label: "Market Value", align: "right" },
+		{ id: "accountPercent", label: "% of Account", align: "right" },
+		{ id: "accountId", label: "Owner" },
+		{ id: "", label: "Recommendation" },
+		{ id: "", label: "" },
+	];
 
-    apis.dashboard
-      .getDashboard()
-      .then((response) => setDashboardData(response))
-      .catch((err) => {
-        toast.error(err.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+	const loadData = async () => {
+		setIsLoading(true);
 
-    apis.accounts
-      .getAll()
-      .then((response) => {
-        setAccounts(response);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-      });
-  };
+		apis.dashboard
+			.getDashboard()
+			.then((response) => setDashboardData(response))
+			.catch((err) => {
+				toast.error(err.message);
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
 
-  React.useEffect(() => {
-    loadData();
-  }, []);
+		apis.accounts
+			.getAll()
+			.then((response) => {
+				setAccounts(response);
+			})
+			.catch((err) => {
+				toast.error(err.message);
+			});
+	};
 
-  return (
-    <>
-      <DashboardTable
-        isLoading={isLoading}
-        refreshData={loadData}
-        rows={dashboardData}
-        accounts={accounts}
-        columns={columns}
-      />
-      <WatchlistSection />
-    </>
-  );
+	React.useEffect(() => {
+		loadData();
+	}, []);
+
+	return (
+		<>
+			<DashboardTable
+				isLoading={isLoading}
+				refreshData={loadData}
+				rows={dashboardData}
+				accounts={accounts}
+				columns={columns}
+			/>
+			<WatchlistSection />
+		</>
+	);
 }
